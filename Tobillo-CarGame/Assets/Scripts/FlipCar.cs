3278c6929079d5502c022e97d6445ecf8ef82037
Raúl <raulblas@ucm.es>
2022-12-06 14:53:20 +0100

    IA rayada
    Co-Authored-By: auroraGJ <48923631+auroraGJ@users.noreply.github.com>

---
+++ Tobillo-CarGame/Assets/Scripts/FlipCar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCar : MonoBehaviour
+{
+    Rigidbody rb;
+    float lastTimeChecked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.up.y > 0.5f || rb.velocity.magnitude > 1)
+        {
+            lastTimeChecked = Time.time;
+        }
+
+        if(Time.time > lastTimeChecked + 3)
+        {
+            RighCar();
+        }
+    }
+
+    void RighCar()
+    {
+        transform.position += Vector3.up;
+        transform.rotation = Quaternion.LookRotation(transform.forward);
+    }
+}
